5e833178b0b9fd6fe1e132ad5bda670174aab516
nishant <nishantpriya.scorpion@gmail.com>
2025-03-16 22:00:09 +0530

    started project

---
+++ server.js
@@ -0,0 +1,8 @@
+// console.log("i am in contact manger aaa")
+const express = require("express")
+const app = express();
+const PORT = 8000;
+
+app.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`)
+})
